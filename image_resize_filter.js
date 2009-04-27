f349f8aa891ca13d6c5a9f786f627472dd369f85
Nathan Haug <quicksketch@35821.no-reply.drupal.org>
2009-04-27 23:51:05 +0000

    #446724: marquardt: Adding rel attribute to links.

--- image_resize_filter.js
+++ image_resize_filter.js
@@ -7,16 +7,19 @@
  * Show the link class option if the "Link to the original" option is checked.
  */
 jQuery(document).ready(function() {
-  jQuery('.image-resize-filter-link-options input').change(function() {
+  jQuery('.image-resize-filter-link-options input.form-checkbox').change(function() {
     if (this.checked) {
+      jQuery('span.image-resize-filter-rel').show();
       jQuery('span.image-resize-filter-class').show();
     }
     else {
+      jQuery('span.image-resize-filter-rel').hide();
       jQuery('span.image-resize-filter-class').hide();
     }
   });
 
   if (jQuery('.image-resize-filter-link-options input').is('[checked]') == false) {
+    jQuery('span.image-resize-filter-rel').hide();
     jQuery('span.image-resize-filter-class').hide();
   }
 })
